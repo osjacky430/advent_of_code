af0bfe97efc9e4797a3a9bed49fb4b167ca36084
Jacky Tseng <osjacky430@gmaail.com>
2023-12-09 22:40:19 +0800

    day 2 solution

--- include/string_util.hpp
+++ include/string_util.hpp
@@ -5,10 +5,9 @@
 #include <range/v3/view/transform.hpp>
 #include <ranges>
 #include <string>
-#include <string_view>
 #include <vector>
 
-inline auto split_string(std::string const& t_str, char t_delim = ' ') {
+[[nodiscard]] inline auto split_string(std::string const& t_str, char t_delim = ' ') {
   using ranges::views::transform, ranges::views::split, ranges::to_vector, ranges::distance;
   auto ret_val = t_str | split(t_delim) | transform([](auto&& t_rng) {
                    return std::string{&*t_rng.begin(), static_cast<std::size_t>(distance(t_rng))};
@@ -20,8 +19,16 @@ inline auto split_string(std::string const& t_str, char t_delim = ' ') {
 
 template <typename StrType>
   requires(std::ranges::contiguous_range<StrType>)
-inline constexpr std::from_chars_result ranges_from_chars(StrType&& t_rng, int& t_res, int const t_base = 10) {
-  auto const* const first = std::ranges::data(t_rng);
-  auto const* const last  = first + std::ranges::size(t_rng);
-  return std::from_chars(first, last, t_res, t_base);
+inline constexpr auto ranges_from_chars(StrType&& t_rng, int& t_res, int const t_base = 10) {
+  if constexpr (ranges::enable_view<std::remove_cvref_t<StrType>>) {
+    auto const* const first = std::ranges::data(t_rng);
+    auto const* const last  = first + std::ranges::size(t_rng);
+    auto const [ptr, err]   = std::from_chars(first, last, t_res, t_base);
+    return std::make_pair(std::span(ptr, last), err);
+  } else {
+    auto const* const first = std::ranges::data(std::forward<StrType>(t_rng));
+    auto const* const last  = first + std::ranges::size(std::forward<StrType>(t_rng));
+    auto const [ptr, err]   = std::from_chars(first, last, t_res, t_base);
+    return std::make_pair(std::span(ptr, last), err);
+  }
 }
---
+++ src/2023/day2/day2.cpp
@@ -0,0 +1,110 @@
+#include "string_util.hpp"
+#include <array>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fstream>
+#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/view/enumerate.hpp>
+#include <range/v3/view/getlines.hpp>
+#include <range/v3/view/transform.hpp>
+#include <range/v3/view/trim.hpp>
+
+using namespace std::string_view_literals;
+
+void part1() {
+  using ranges::getlines, ranges::views::transform, ranges::accumulate, ranges::views::enumerate, ranges::views::trim;
+
+  std::fstream in((INPUT_FILE));
+  auto rng = getlines(in);
+
+  static constexpr auto CUBES_COUNT = std::array{12, 13, 14};  // RED, GREEN, BLUE
+
+  constexpr auto get_cubes_grabbed = [](auto const& t_str) {
+    auto const game = split_string(t_str, ':');
+    auto const sets = split_string(game[1], ';');
+
+    for (auto const& set : sets | transform([](auto const& t_str) { return split_string(t_str, ','); })) {
+      for (auto const& ball : set) {
+        int quantity           = 0;
+        auto trimmed           = ball | trim([](auto&& t_str) { return std::isspace(t_str); });
+        auto const [rest, err] = ranges_from_chars(trimmed, quantity);
+        if (err != std::errc() or rest.empty()) {
+          return 0;
+        }
+
+        auto const idx = [=]() {
+          switch (rest[1]) {
+            case 'r':
+              return 0UL;
+            case 'g':
+              return 1UL;
+            case 'b':
+              return 2UL;
+          }
+
+          std::unreachable();
+        }();
+
+        if (quantity > CUBES_COUNT[idx]) {
+          return 0;
+        }
+      }
+    }
+
+    int ret_val = 0;
+    ranges_from_chars(std::string_view(game[0]).substr(std::size("Game "sv)), ret_val);
+    return ret_val;
+  };
+
+  fmt::println("id sum: {}", accumulate(rng | transform(get_cubes_grabbed), 0));
+}
+
+void part2() {
+  using ranges::getlines, ranges::views::transform, ranges::accumulate, ranges::views::enumerate, ranges::views::trim;
+
+  std::fstream in((INPUT_FILE));
+  auto rng = getlines(in);
+
+  constexpr auto get_cubes_power = [](auto const& t_str) {
+    auto const game = split_string(t_str, ':');
+    auto const sets = split_string(game[1], ';');
+
+    int max_power = 0;
+    std::array<int, 3> result{0, 0, 0};
+
+    for (auto const& set : sets | transform([](auto const& t_str) { return split_string(t_str, ','); })) {
+      for (auto const& ball : set) {
+        int quantity         = 0;
+        auto trimmed         = ball | trim([](auto&& t_str) { return std::isspace(t_str); });
+        auto const [rest, _] = ranges_from_chars(trimmed, quantity);
+        auto const idx       = [=]() {
+          switch (rest[1]) {
+            case 'r':
+              return 0UL;
+            case 'g':
+              return 1UL;
+            case 'b':
+              return 2UL;
+          }
+
+          std::unreachable();
+        }();
+
+        result[idx] = std::max(result[idx], quantity);
+      }
+    }
+
+    max_power = std::max(max_power, accumulate(result, 1, std::multiplies<>{}));
+
+    return max_power;
+  };
+
+  fmt::println("power sum: {}", accumulate(rng | transform(get_cubes_power), 0));
+}
+
+int main(int /**/, char** /**/) {
+  part1();
+  part2();
+
+  return EXIT_SUCCESS;
+}
